3e625cda06423486a4920560837c7a045b4e5327
deepakkumar-it19 <deepakkumar.it19@bitsathy.ac.in>
2019-10-01 15:05:21 +0530

    first commit

---
+++ table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a,i,b;
+     a=3;
+    for(i=1;i<=10;i++)
+    {
+        b=i*a;
+        printf("%d*3=%d",i,b);
+         printf("\n");
+    }
+    
+    return 0;
+}
